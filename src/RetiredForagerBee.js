4fe99feea158cd87fb14e10c1a9291f550fe069a
Jeff Chasin <jeff@jeffchasin.com>
2018-08-09 15:50:00 -0400

    6ees6ees6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,21 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends Grub {
   // TODO..
+  constructor() {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    this.food = 'jelly';
+    this.treasureChest = [];
+  }
+
+  forage() {
+    return "I am too old, let me play cards instead";
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
+
 };
